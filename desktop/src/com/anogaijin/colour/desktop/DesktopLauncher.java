bfed8ade07853fea3155fbf4ecf2b322e69381db
Adrian <adunne@expedia.com>
2015-10-17 23:19:34 +0900

    Empty project

---
+++ desktop/src/com/anogaijin/colour/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.anogaijin.colour.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.anogaijin.colour.Colour;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Colour(), config);
+	}
+}
